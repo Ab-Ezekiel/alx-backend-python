a4ca2d26fe1123595af7e2abaff043fb7c050a31
Ab-Ezekiel <akpanabraham1000@gmail.com>
2025-09-25 20:43:45 +0100

    test: add mocked tests for utils.get_json with parameterized inputs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+from unittest.mock import patch, Mock   # <-- add this import here
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -23,6 +24,22 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         self.assertEqual(str(cm.exception), repr(path[-1]))
 
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch("utils.requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        from utils import get_json
+        result = get_json(test_url)
+
+        self.assertEqual(result, test_payload)
+        mock_get.assert_called_once_with(test_url)
 
 if __name__ == "__main__":
     unittest.main()
