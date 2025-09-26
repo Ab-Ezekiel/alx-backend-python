1746a549ed55c8aa8c546c40423519d3e87e294b
Ab-Ezekiel <akpanabraham1000@gmail.com>
2025-09-26 23:35:27 +0100

    chore(routing): add NestedDefaultRouter for nested conversation messages and add api-auth to project urls

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,9 +1,15 @@
 # messaging_app/chats/urls.py
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
+# top-level router
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+router.register(r'messages', MessageViewSet, basename='message')  # optional top-level messages
 
-urlpatterns = router.urls
+# nested router: /conversations/{conversation_pk}/messages/
+conversations_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+conversations_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
+urlpatterns = router.urls + conversations_router.urls
