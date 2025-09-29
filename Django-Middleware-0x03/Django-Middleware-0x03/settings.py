07ad79460e4c3acc16d0118d8e873d5122e7d9f5
Ab-Ezekiel <akpanabraham1000@gmail.com>
2025-09-29 21:56:56 +0100

    Fix middleware import issue and restructure project

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -88,9 +88,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    # Custom middleware
+    'chats.middleware.RequestLoggingMiddleware',
+    
 ]
 
-ROOT_URLCONF = 'messaging_app.urls'
+ROOT_URLCONF = 'Django-Middleware-0x03.urls'
 
 TEMPLATES = [
     {
@@ -108,7 +112,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'messaging_app.wsgi.application'
+WSGI_APPLICATION = 'Django-Middleware-0x03.wsgi.application'
 
 
 # Database
